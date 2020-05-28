d3a6b11a463e8aa151367d41a55c5af7652c790a
Alireza Akrami <akrami.ui@gmail.com>
2020-05-28 16:49:08 +0430

    wireframe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cities: [
+        { name: 'Tehrān', woeid: 2251945, d: 35, w: 'c' },
+        { name: 'San Francisco', woeid: 2487956, d: 22, w: 'hc' },
+        { name: 'London', woeid: 44418, d: 12, w: 'lc' },
+        { name: 'Munich', woeid: 676757, d: -2, w: 'sn' }
+      ],
+      current: 0
+    }
+  }
+
+  setCurrent(id) {
+    this.setState({ current: id });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.cities[this.state.current].name}</h1>
+        <h2>{this.state.cities[this.state.current].d}°C</h2>
+        <h3>{this.state.cities[this.state.current].w}</h3>
+
+        <div id="cities">
+          {this.state.cities.map((each, index) => {
+            return (
+              <button key={each.woeid} onClick={() => this.setCurrent(index)} display="inline">
+                <h3>{each.name}</h3>
+                <h5>{each.d}°C</h5>
+              </button>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
